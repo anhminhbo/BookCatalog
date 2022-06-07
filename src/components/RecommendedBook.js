ea88f3727f72ea5a572264b42423b6c586e1c25c
Minh Nguyens <minhnguyen@wee.vn>
2022-06-07 16:03:04 +0700

    update pubYear for recommended book

--- src/components/RecommendedBook.js
+++ src/components/RecommendedBook.js
@@ -3,7 +3,7 @@ const RecommendedBook = ({ recommendedBook }) => {
     <>
       <h3>Name: {recommendedBook.name}</h3>
       <p>Author: {recommendedBook.author}</p>
-
+      <p>Publication year: {recommendedBook.pubYear}</p>
       <p> Rating: {recommendedBook.rating} </p>
       <p> ISBN: {recommendedBook.isbn}</p>
     </>
